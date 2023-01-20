eb40de2f05462c0521b6fce329cb005341ec58ac
BantuMasyarakat <khusainimukhamad@gmail.com>
2023-01-20 10:54:17 +0700

    add something

--- components/DraftEditorSide.tsx
+++ components/DraftEditorSide.tsx
@@ -63,7 +63,7 @@ export default function DraftEditorSide(props: DraftEditorSideProps) {
     };
 
     const loader = ({ src, quality, width }: any) => {
-        return `https://${process.env.baseurl}/${src}?w=${width}&q=${quality || 75}`;
+        return `https://${process.env.BASE_URL}/${src}?w=${width}&q=${quality || 75}`;
     };
 
     return (
