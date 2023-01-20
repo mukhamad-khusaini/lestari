0a44a22297c9e9d874a659a6f6a4630da6f8c646
BantuMasyarakat <khusainimukhamad@gmail.com>
2023-01-20 11:05:56 +0700

    add string

--- components/DraftEditorSide.tsx
+++ components/DraftEditorSide.tsx
@@ -63,7 +63,7 @@ export default function DraftEditorSide(props: DraftEditorSideProps) {
     };
 
     const loader = ({ src, quality, width }: any) => {
-        return `${process.env.BASE_URL}/${src}?w=${width}&q=${quality || 75}`;
+        return `https://lestari.vercel.app/${src}?w=${width}&q=${quality || 75}`;
     };
 
     return (
