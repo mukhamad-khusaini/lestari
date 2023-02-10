df7a0529ae45a4d563bb7fd4b37da91ef29e11a6
Mukhamad Khusaini <67376611+mukhamad-khusaini@users.noreply.github.com>
2023-02-10 11:33:11 +0700

    Update DraftList.tsx

--- components/DraftList.tsx
+++ components/DraftList.tsx
@@ -64,18 +64,7 @@ export default function DraftList() {
                             minute: "2-digit",
                         })}
                     />
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2402370305885827"
-     crossorigin="anonymous"></script>
-<!-- Iki Iklan -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2402370305885827"
-     data-ad-slot="7875159975"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                   
                 );
             })}
         </div>
