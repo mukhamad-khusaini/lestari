8eea78af528ba6a28bffcef34000dacf86e66de9
Mukhamad Khusaini <67376611+mukhamad-khusaini@users.noreply.github.com>
2023-02-10 11:34:45 +0700

    Update index.tsx

--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -21,18 +21,7 @@ export default function EditorPage() {
                 <div className={styles.draftContainer}>
                     <DraftList />
                 </div>
-                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2402370305885827"
-     crossorigin="anonymous"></script>
-<!-- Iki Iklan -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-2402370305885827"
-     data-ad-slot="7875159975"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+                 
             </main>
         </>
     );
